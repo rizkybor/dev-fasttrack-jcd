852bf8040571ba9196e5ce7ec3f33d81f9b46b0c
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2026-05-27 13:22:36 +0700

    update-marquee

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,20 @@ export default {
             fontFamily: {
                 sans: ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
             },
+            keyframes: {
+                marquee: {
+                    from: { transform: 'translateX(0)' },
+                    to: { transform: 'translateX(-50%)' },
+                },
+                'marquee-reverse': {
+                    from: { transform: 'translateX(-50%)' },
+                    to: { transform: 'translateX(0)' },
+                },
+            },
+            animation: {
+                'marquee': 'marquee 45s linear infinite',
+                'marquee-reverse': 'marquee-reverse 45s linear infinite',
+            },
         },
     },
 
